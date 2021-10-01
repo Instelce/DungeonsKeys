962f9d1acb59c9cecc26adb41183d768c2c1e4b6
Instelce <intelce.developpement@gmail.com>
2021-10-01 18:57:21 +0200

    Setting up the project

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+    '                  ',
+    '                  ',
+    '      XXX         ',
+    '                  ',
+    'XXX          XXX  ',
+    '                  ',
+    '      XXX         ',
+    '                  ',
+    '        XXX       ',
+    '                  ',
+    '                  ']
+
+tile_size = 64
+screen_width = 500
+screen_height = len(level_map) * tile_size
